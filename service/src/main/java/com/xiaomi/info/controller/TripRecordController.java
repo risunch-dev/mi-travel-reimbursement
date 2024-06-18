9f03625d50c1d0521b16b557c38aaa035a86bef2
zhuad <310129618@qq.com>
2024-06-18 13:30:34 +0800

    feat:接口入参校验、出参统一包装part3.

--- service/src/main/java/com/xiaomi/info/controller/TripRecordController.java
+++ service/src/main/java/com/xiaomi/info/controller/TripRecordController.java
@@ -1,14 +1,19 @@
 package com.xiaomi.info.controller;
 
+import com.xiaomi.info.common.enums.ErrorCodes;
+import com.xiaomi.info.exception.BasicRunException;
 import com.xiaomi.info.response.Response;
 import com.xiaomi.info.service.TripRecordService;
 import com.xiaomi.info.travel.response.ProcessQueryListResponse;
 import com.xiaomi.info.travel.request.ProcessApproveRequest;
 import com.xiaomi.info.travel.request.ProcessQueryListRequest;
 import com.xiaomi.info.travel.request.TravelDeleteRequest;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 
 /**
  * ClassName: TripRecordController
@@ -20,7 +25,7 @@ import javax.annotation.Resource;
  * @Version 1.0
  */
 @RestController
-@RequestMapping("/api/travel/apply")
+@RequestMapping("/api/travel/apply/process")
 public class TripRecordController {
 
     @Resource
@@ -33,7 +38,10 @@ public class TripRecordController {
      */
     @PostMapping("/submit")
     public Response<Boolean> submit(@RequestBody TravelDeleteRequest request) {
-        return Response.success(tripRecordService.submit(request.getId()));
+        // 入参校验
+        checkTripRecordSubmitParameters(request);
+        Boolean result = tripRecordService.submit(request.getId());
+        return Boolean.TRUE.equals(result) ? Response.success(true) : Response.error("启动流程实例失败");
     }
 
     /**
@@ -43,6 +51,8 @@ public class TripRecordController {
      */
     @PostMapping ("/queryList")
     public Response<ProcessQueryListResponse> queryList(@RequestBody ProcessQueryListRequest request) {
+        // 入参校验
+        checkTripRecordQueryListParameters(request);
         ProcessQueryListResponse response = ProcessQueryListResponse.builder()
                 .list(tripRecordService.query(request.getId()))
                 .build();
@@ -56,7 +66,10 @@ public class TripRecordController {
      */
     @PostMapping("/approve")
     public Response<Boolean> approve(@RequestBody ProcessApproveRequest request) {
-        return Response.success(tripRecordService.approve(request));
+        // 入参校验
+        checkTripRecordApproveParameters(request);
+        Boolean result = tripRecordService.approve(request);
+        return Boolean.TRUE.equals(result) ? Response.success(true) : Response.error("审批通过失败");
     }
 
     /**
@@ -66,6 +79,55 @@ public class TripRecordController {
      */
     @PostMapping("/reject")
     public Response<Boolean> reject(@RequestBody ProcessApproveRequest request) {
-        return Response.success(tripRecordService.reject(request));
+        // 入参校验
+        checkTripRecordApproveParameters(request);
+        Boolean result = tripRecordService.reject(request);
+        return Boolean.TRUE.equals(result) ? Response.success(true) : Response.error("审批驳回失败");
     }
+
+    /**
+     * 启动流程实例参数校验
+     * @param request
+     */
+    public void checkTripRecordSubmitParameters(TravelDeleteRequest request) {
+        if (Objects.isNull(request)) {
+            throw new BasicRunException(ErrorCodes.MISSING_PARAMETER.getCode(), "请求参数不能为空");
+        }
+        if (Objects.isNull(request.getId()) || request.getId() <= NumberUtils.LONG_ZERO) {
+            throw new BasicRunException(ErrorCodes.BAD_PARAMETERS.getCode(), "差旅申请id不合法");
+        }
+    }
+
+    /**
+     * 查看个人代办任务参数校验
+     * @param request
+     */
+    public void checkTripRecordQueryListParameters(ProcessQueryListRequest request) {
+        if (Objects.isNull(request)) {
+            throw new BasicRunException(ErrorCodes.MISSING_PARAMETER.getCode(), "请求参数不能为空");
+        }
+        if (Objects.isNull(request.getId()) || request.getId() <= NumberUtils.LONG_ZERO) {
+            throw new BasicRunException(ErrorCodes.BAD_PARAMETERS.getCode(), "查看待办任务的userId不合法");
+        }
+    }
+
+    /**
+     * 审批通过/驳回参数校验
+     * @param request
+     */
+    public void checkTripRecordApproveParameters(ProcessApproveRequest request) {
+        if (Objects.isNull(request)) {
+            throw new BasicRunException(ErrorCodes.MISSING_PARAMETER.getCode(), "请求参数不能为空");
+        }
+        if (Objects.isNull(request.getId()) || request.getId() <= NumberUtils.LONG_ZERO) {
+            throw new BasicRunException(ErrorCodes.BAD_PARAMETERS.getCode(), "审批的userId不合法");
+        }
+        if (StringUtils.isBlank(request.getInstanceId())) {
+            throw new BasicRunException(ErrorCodes.BAD_PARAMETERS.getCode(), "流程实例id不能为空");
+        }
+        if (StringUtils.isBlank(request.getDesc())) {
+            throw new BasicRunException(ErrorCodes.BAD_PARAMETERS.getCode(), "审批描述不能为空");
+        }
+    }
+
 }
